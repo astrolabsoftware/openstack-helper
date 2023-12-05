a5fe6433bc3a1de95fc24a62731c85f5d528a770
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-12-05 12:18:59 +0100

    Update current feature

--- boot.sh
+++ boot.sh
@@ -1,5 +1,12 @@
-# WARN this will create a volume which needs to be deleted after
-openstack server create --flavor vd.16 --network public-2 --key-name ssh-key --image Fedora-38 --boot-from-volume 100  --wait fink-dev
-openstack server add security group fink-dev "ssh + icmp"
+#!/bin/bash
+
+set -euxo pipefail
+
+VOLUME_SIZE=100
+VM_NAME="fink-dev"
+
+# WARN this will create a volume which needs to be deleted after vm deletion
+openstack server create --flavor vd.16 --network public-2 --key-name ssh-key --image Fedora-38 --boot-from-volume "$VOLUME_SIZE"  --wait "$VM_NAME"
+openstack server add security group "$VM_NAME" "ssh + icmp"
 
 
